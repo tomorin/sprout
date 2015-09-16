8d1fc35dbcd1962be79c65c0c8341338b2ecafa0
hyoga <hyoga0216@gmail.com>
2015-09-16 13:09:04 +0900

    make Gemfile

---
+++ Gemfile
@@ -0,0 +1,14 @@
+
+source 'http://rubygems.org'
+
+gem 'thin'
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+gem 'json'
+
+gem "sqlite3"
+gem "activerecord"
+
+gem "nokogiri"
+
---
+++ app.rb
@@ -0,0 +1,26 @@
+
+#coding utf-8
+require "sinatra"
+require "sinatra/reloader" if development?
+
+require 'active_record'
+
+require 'nokogiri'
+require 'open-uri'
+
+require 'json'
+
+=begin
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection('development')
+
+class Memos < ActiveRecord::Base
+end
+=end
+
+#########################################################################################
+
+get '/test' do
+	erb: test
+end
+
